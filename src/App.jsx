d8720aa1d60bf49e8978fb255457570b5efe9c06
Patrick Duvall <duvallp208@gmail.com>
2021-03-03 10:30:34 -0700

    Add initial fetch

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,16 @@ class App extends Component {
       { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
     ]
     super();
-    this.state = { ideas: [] }
+    this.state = { ideas: [], error:'', loading: 'Loading...' }
   }
+
+  componentDidMount = () => {
+    // debugger
+    fetch('http://localhost:3001/api/v1/ideas')
+      .then(response => response.json())
+      .then(data => this.setState({ideas: [...data], loading: ''}))
+      .catch(error => this.setState({error: error.message, loading: ''}))
+  } 
 
   addIdea = (newIdea) => {
     this.setState({ ideas: [...this.state.ideas, newIdea] })
@@ -21,7 +29,7 @@ class App extends Component {
 
   deleteIdea = (id) => {
     console.log(id);
-    const filteredIdeas = this.state.ideas.filter(idea => idea.id != id);
+    const filteredIdeas = this.state.ideas.filter(idea => idea.id !== id);
     this.setState({ ideas: filteredIdeas })
   }
 
@@ -30,6 +38,8 @@ class App extends Component {
       <main className='App'>
         <h1>Ideabox</h1>
         <Form addIdea={this.addIdea}/>
+        {this.state.error && <h2>{this.state.error}</h2>}
+        {this.state.loading && <h2>{this.state.loading}</h2>}
         {!this.state.ideas.length && <h2>No ideas yet -- add some!</h2>}
         <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea}/>
       </main>
